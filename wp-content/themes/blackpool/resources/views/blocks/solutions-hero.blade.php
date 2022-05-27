905b96cd6067ad10dc93a02c3d501c7deec8fc36
Gemma Robinson <gemma@vidacreative.co.uk>
2022-05-27 22:15:06 +0100

    edits to solutions hero

--- wp-content/themes/blackpool/resources/views/blocks/solutions-hero.blade.php
+++ wp-content/themes/blackpool/resources/views/blocks/solutions-hero.blade.php
@@ -10,17 +10,21 @@ PostTypes: page
 SupportsAlign: full
 SupportsMultiple: false
 --}}
-<section data-{{ $block['id'] }} class="{{ $block['classes'] }}">
+@php($color = get_field('colour'))
+<section data-{{ $block['id'] }} class="{{ $block['classes'] }} @if($color) solutions-hero--{{ $color }} @endif">
     <div class="container">
-        <div class="row justify-content-end">
+        <div class="row @if(get_field('image_position')) justify-content-end @endif">
             <div class="col-24 col-lg-14 col-xl-13 col-xxl-12">
                 <h1>{{ get_field('heading') }}</h1>
+                @if($site = get_field('website'))
+                    <a class="solutions-hero__link" href="{{ $site }}" target="_blank">{{ $site }}</a>
+                @endif
                 <p class="bold-text mb-0">{{ get_field('subheading') }}</p>
             </div>
         </div>
     </div>
     @if($img = get_field('image'))
-        <div class="solutions-hero__img">
+        <div class="solutions-hero__img @if(!get_field('image_position')) solutions-hero__img--right @endif">
             <img src="{{ $img['url'] }}" alt="{{ $img['alt'] }}" />
         </div>
     @endif
